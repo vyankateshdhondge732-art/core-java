e59fc6e399bf403db410b7203aec0731acf04efa
Vyankatesh Dhondge <vyankateshdhondge732@gmail.com>
2026-01-03 00:25:02 +0530

    java

---
+++ krishana/src/krishana/second.java
@@ -0,0 +1,32 @@
+package krishana;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class second {
+     public static void main (String[]args) {
+    	 
+    	 
+    	 
+    	 
+    	 List<Integer>List= new ArrayList<Integer>();
+    	 
+    	 List .add(70);
+    	 List.add(60);
+    	 List.add(90);
+    	 List.add(70);
+    	 List.add(60);
+    	 List.add(40);
+      for  (int i=0;i<list.size();i++);
+      {
+    	  for(int j=i+1;j<list.size();j++)
+    	  {
+         if(list.get(i))==list.get(j);
+         {
+        	 list2.add(list.get(i))
+         }
+      
+	
+	System.out.println(list2);
+	
+} 
