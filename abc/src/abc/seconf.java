fceb3c17b5808524d680bc01bb3999329781b5a7
Vyankatesh Dhondge <vyankateshdhondge732@gmail.com>
2026-01-07 00:09:29 +0530

    java

---
+++ abc/src/abc/seconf.java
@@ -0,0 +1,16 @@
+package abc;
+
+public class seconf {
+
+	public static void main(String[] args) {
+     for( int i =1; i<=100;i++){
+    	 if(1%2==0) {
+    		 System.out.print(i + "");
+    	 }
+     }
+     System.out.println(9);
+}
+}
+	
+
+
