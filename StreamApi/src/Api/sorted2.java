50ec383791cdecee809aa8e4e77862cd369700bd
Vyankatesh Dhondge <vyankateshdhondge732@gmail.com>
2026-01-14 22:08:47 +0530

    stream

---
+++ StreamApi/src/Api/sorted2.java
@@ -0,0 +1,15 @@
+package Api;
+
+import java.util.List;
+
+public class sorted2 {
+
+	public static void main(String[] args) {
+      List.of("kfjj","uhyfu","jbbg")
+      .stream()
+      .map((s)->{return s.length();})
+      .forEach((i)->{System.out.println(i);});
+      
+   	}
+
+}
