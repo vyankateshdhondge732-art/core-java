233683c0c8e2f72d4cb3a2d73fe8d02fca9f7a5c
Vyankatesh Dhondge <vyankateshdhondge732@gmail.com>
2026-01-17 23:05:50 +0530

    stream

---
+++ Sorted_1/src/api/second.java
@@ -0,0 +1,20 @@
+package api;
+
+import java.util.List;
+
+public class second {
+
+	public static void main(String[] args) {
+          
+  
+		
+		   List.of("uiui","kjgj","kjjf")
+		  .stream()
+		  .map((s)->{return s.length();})
+		  .forEach((i)->{System.out.println(i);});
+
+           
+
+	}
+
+}
