710c0421dafea38cdf50929b1c3d6f284f628ba1
Vyankatesh Dhondge <vyankateshdhondge732@gmail.com>
2026-01-26 14:35:36 +0530

    stream

---
+++ Sorted_1/src/api/five.java
@@ -0,0 +1,15 @@
+package api;
+
+import java.util.List;
+
+public class five {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         List.of("djfhjf","dfmm","fkfj")
+         .stream()
+         .map(i->String.valueOf(0))
+         .forEach(i->System.out.println(i));
+	}
+
+}
