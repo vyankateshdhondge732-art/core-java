eca0b7b5c15bdb0cb6b41b53ddcf366a23bed645
Vyankatesh Dhondge <vyankateshdhondge732@gmail.com>
2026-01-27 22:47:41 +0530

    Entryset

---
+++ Methods/src/Refer/entryset.java
@@ -0,0 +1,14 @@
+package Refer;
+
+import java.util.List;
+
+public class entryset {
+
+	public static void main(String[] args) {
+          
+		List.of("abcd","ajhhd","skdh")
+		.stream()
+		.collect(Collectors.)
+	}
+
+}
