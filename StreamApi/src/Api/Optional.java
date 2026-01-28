2a337a4eab267fa30d7a5a99d4e6916b0aaa3aee
Vyankatesh Dhondge <vyankateshdhondge732@gmail.com>
2026-01-28 23:43:56 +0530

    optiona

---
+++ StreamApi/src/Api/Optional.java
@@ -0,0 +1,19 @@
+package Api;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Optional {  
+
+	public static void main(String[] args) {
+           
+		List<Integer>list=new ArrayList<Integer>();
+		list.add(12);
+		list.add(13);
+		list.add(null);
+		
+		Optional<Integer> op =list
+		
+		}
+
+}
